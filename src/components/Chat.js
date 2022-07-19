a00f21fa96d8ff11b017d5d2c9e44118b58b22ff
DELL\necoj <nemanja.jovic195@gmail.com>
2022-07-19 19:22:46 +0200

    Added Chat logic

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+
+import { db, auth } from "../firebase";
+
+import SignOut from "./SignOut";
+import Message from "./Message";
 
 const Chat = () => {
-  return <div>Chat</div>;
+  const scroll = useRef();
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    db.collection("messages")
+      .orderBy("createdAt")
+      .limit(50)
+      .onSnapshot((snapshot) => {
+        setMessages(snapshot.docs.map((doc) => doc.data()));
+      });
+  }, []);
+
+  return (
+    <div>
+      <SignOut />
+      <div>
+        {messages.map(({ id, text, photoURL }) => (
+          <div>
+            <div key={id}>
+              <img src={photoURL} alt="profileImage" />
+              <p>{text}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <Message scroll={scroll} />
+      <div ref={scroll}></div>
+    </div>
+  );
 };
 
 export default Chat;
