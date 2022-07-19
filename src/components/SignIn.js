57e42952f6316b388d9e94ece51ae28603abd517
DELL\necoj <nemanja.jovic195@gmail.com>
2022-07-19 19:21:58 +0200

    Added sign in functionality

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,9 +1,16 @@
-import React from "react";
+import firebase from "firebase";
+
+import { auth } from "../firebase";
 
 const SignIn = () => {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+
   return (
     <div>
-      <button>Sign In with Google</button>
+      <button onClick={signInWithGoogle}>Sign In with Google</button>
     </div>
   );
 };
