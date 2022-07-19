941d240bf44d63753a147094534964c11780b21b
DELL\necoj <nemanja.jovic195@gmail.com>
2022-07-19 19:26:54 +0200

    Added send message functionality

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { db, auth } from "../firebase";
+import firebase from "firebase";
 
-const Message = () => {
-  return <div>Message</div>;
+const SendMessage = ({ scroll }) => {
+  const [text, setText] = useState("");
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+    const { uid, photoURL } = auth.currentUser;
+
+    await db.collection("messages").add({
+      text,
+      photoURL,
+      uid,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+    setText("");
+    scroll.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  return (
+    <div>
+      <form onSubmit={sendMessage}>
+        <div className="sendMsg">
+          <input
+            placeholder="Message..."
+            type="text"
+            value={text}
+            onChange={(e) => setMsg(e.target.value)}
+          />
+          <button type="submit">Send</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
-export default Message;
+export default SendMessage;
