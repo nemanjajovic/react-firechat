72dca6ea753bb371f81cbccf108baa356ee5f604
DELL\necoj <nemanja.jovic195@gmail.com>
2022-07-19 19:22:20 +0200

    Added sign out functionality

--- src/components/SignOut.js
+++ src/components/SignOut.js
@@ -1,7 +1,11 @@
-import React from "react";
+import { auth } from "../firebase";
 
 const SignOut = () => {
-  return <div>SignOut</div>;
+  return (
+    <div>
+      <button onClick={() => auth.signOut()}>Sign Out</button>
+    </div>
+  );
 };
 
 export default SignOut;
